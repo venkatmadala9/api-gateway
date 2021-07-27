50bedb483cd411c05a058f61c526f2f6f1ebcb64
vmadala <vmadala@VMADALA-PC.pkrmsezdc.com>
2021-07-27 14:14:02 +0530

    2707

--- api-gateway/src/main/java/com/lodging/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/lodging/api/gateway/ApiGatewayApplication.java
@@ -2,18 +2,29 @@ package com.lodging.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-@CircuitBreaker(name = "CircuitBreaker")
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder)
+	{
+		return routeLocatorBuilder.routes().route("USER", r -> r.path("/user/**").uri("lb://USER-SERVICE"))
+											.route("HOTEL", r -> r.path("/hotel/**").uri("lb://HOTEL-SERVICE"))
+											.build();
+	}
+	
+	@Bean
+	
 }
